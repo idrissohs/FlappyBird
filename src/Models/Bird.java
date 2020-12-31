96e321003fabae6612657da63cb7e95dd059792f
itsmaarouf <github@coved19.de>
2020-12-31 10:04:29 +0100

    create models (Bird & Tubes)

---
+++ src/Models/Bird.java
@@ -0,0 +1,27 @@
+package Models;
+
+public class Bird {
+    int posisionY;
+    int flyUp;
+
+    public Bird(int posisionY, int flyUp) {
+        this.posisionY = posisionY;
+        this.flyUp = flyUp;
+    }
+
+    public int getPosisionY() {
+        return posisionY;
+    }
+
+    public void setPosisionY(int posisionY) {
+        this.posisionY = posisionY;
+    }
+
+    public int getFlyUp() {
+        return flyUp;
+    }
+
+    public void setFlyUp(int flyUp) {
+        this.flyUp = flyUp;
+    }
+}
---
+++ src/Models/Tubes.java
@@ -0,0 +1,47 @@
+package Models;
+
+public class Tubes {
+    int Height;
+    int Weight;
+    int posisionX;
+    int posisionY;
+
+    public Tubes(int height, int weight, int posisionX, int posisionY) {
+        Height = height;
+        Weight = weight;
+        this.posisionX = posisionX;
+        this.posisionY = posisionY;
+    }
+
+    public int getHeight() {
+        return Height;
+    }
+
+    public void setHeight(int height) {
+        Height = height;
+    }
+
+    public int getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(int weight) {
+        Weight = weight;
+    }
+
+    public int getPosisionX() {
+        return posisionX;
+    }
+
+    public void setPosisionX(int posisionX) {
+        this.posisionX = posisionX;
+    }
+
+    public int getPosisionY() {
+        return posisionY;
+    }
+
+    public void setPosisionY(int posisionY) {
+        this.posisionY = posisionY;
+    }
+}
