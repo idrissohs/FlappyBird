7fb543aa9cad9f50cb68af0b7cbbe00d9a85828a
itsmaarouf <github@coved19.de>
2021-01-08 10:23:52 +0100

    game

--- src/models/modelsImpl/Game.java
+++ src/models/modelsImpl/Game.java
@@ -1,35 +1,61 @@
 package models.modelsImpl;
 
 public class Game {
-    private cell[][]  grid;
-    private int size = 10;
 
-    public Game(){
-        this.grid = new cell[10][20];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 20; j++) {
-                this.grid[i][j] = new cell("  ");
-            }
-        }
-    }
 
-    public cell getCell(int i,int j){
-        return this.grid[i][j];
-    }
+//    private cell[][]  grid;
+//    private int size = 6;
+//
+//    public Game(){
+//        this.grid = new cell[this.size][this.size * 2];
+//        for (int i = 0; i < this.size; i++) {
+//            for (int j = 0; j < this.size * 2; j++) {
+//                this.grid[i][j] = new cell("  ");
+//            }
+//        }
+//    }
+//
+//    public cell getCell(int i,int j){
+//        return this.grid[i][j];
+//    }
 
     /*public  String toString(){
-        String result0 = "|____________________________________________|\n";
-        String result1 = "|";
-        String resultF = "";
+        String result = "";
+        String floor= "|__|__|__|__|__|__|__|__|__|__|__|\n";
 
-        this.grid = new cell[10][20];
-        for (int i = 0; i < 10; i++) {
-            System.out.println(result0);
-            for (int j = 0; j < 20; j++) {
-                this.grid[i][j] = new cell(result1);
+        System.out.println(result);
+
+        this.grid = new cell[this.size][this.size * 2];
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size *2; j++) {
+
+                String V = "|" + this.grid[i][j];
+
+               // this.grid[i][j] = new cell(V);
+                System.out.print("|  ");
             }
+            System.out.println("\n" +floor);
         }
 
-        return result0 +result1;
+        return result;
     }*/
+
+//    public  String toStringV1(){
+//        String result = "_____________________________________________________________\n";
+//        result += "|" + this.grid[0][0].toString() + "|" + this.grid[0][1].toString()  + "|" + this.grid[0][2].toString()  + "|" + this.grid[0][3].toString()  + "|" + this.grid[0][4].toString()  + "|" + this.grid[0][5].toString()  + "|" + this.grid[0][6].toString()  + "|" + this.grid[0][7].toString()  + "|" + this.grid[0][8].toString()  + "|" + this.grid[0][9].toString()  + "|" + this.grid[0][10].toString()  + "|" + this.grid[0][11].toString()  + "|" + this.grid[0][12].toString() ;
+//        result += "|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n";
+//        result += "|" + this.grid[1][0].toString() + "|" + this.grid[1][1].toString()  + "|" + this.grid[1][2].toString()  + "|" + this.grid[1][3].toString()  + "|" + this.grid[1][4].toString()  + "|" + this.grid[1][5].toString()  + "|" + this.grid[1][6].toString()  + "|" + this.grid[1][7].toString()  + "|" + this.grid[1][8].toString()  + "|" + this.grid[1][9].toString()  + "|" + this.grid[1][10].toString()  + "|" + this.grid[1][11].toString()  + "|" + this.grid[1][12].toString()  ;
+//        result += "|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n";
+//        result += "|" + this.grid[2][0].toString() + "|" + this.grid[2][1].toString()  + "|" + this.grid[1][2].toString()  + "|" + this.grid[1][3].toString()  + "|" + this.grid[1][4].toString()  + "|" + this.grid[1][5].toString()  + "|" + this.grid[1][6].toString()  + "|" + this.grid[1][7].toString()  + "|" + this.grid[1][8].toString()  + "|" + this.grid[1][9].toString()  + "|" + this.grid[1][10].toString()  + "|" + this.grid[1][11].toString()  + "|" + this.grid[1][12].toString()  ;
+//        result += "|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n";
+//        result += "|" + this.grid[3][0].toString() + "|" + this.grid[3][1].toString()  + "|" + this.grid[1][2].toString()  + "|" + this.grid[1][3].toString()  + "|" + this.grid[1][4].toString()  + "|" + this.grid[1][5].toString()  + "|" + this.grid[1][6].toString()  + "|" + this.grid[1][7].toString()  + "|" + this.grid[1][8].toString()  + "|" + this.grid[1][9].toString()  + "|" + this.grid[1][10].toString()  + "|" + this.grid[1][11].toString()  + "|" + this.grid[1][12].toString()  ;
+//        result += "|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n";
+//        result += "|" + this.grid[4][0].toString() + "|" + this.grid[4][1].toString()  + "|" + this.grid[4][2].toString()  + "|" + this.grid[1][3].toString()  + "|" + this.grid[1][4].toString()  + "|" + this.grid[1][5].toString()  + "|" + this.grid[1][6].toString()  + "|" + this.grid[1][7].toString()  + "|" + this.grid[1][8].toString()  + "|" + this.grid[1][9].toString()  + "|" + this.grid[1][10].toString()  + "|" + this.grid[1][11].toString()  + "|" + this.grid[1][12].toString()  ;
+//        result += "|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n";
+//        result += "|" + this.grid[5][0].toString() + "|" + this.grid[5][1].toString()  + "|" + this.grid[5][2].toString()  + "|" + this.grid[1][3].toString()  + "|" + this.grid[1][4].toString()  + "|" + this.grid[1][5].toString()  + "|" + this.grid[1][6].toString()  + "|" + this.grid[1][7].toString()  + "|" + this.grid[1][8].toString()  + "|" + this.grid[1][9].toString()  + "|" + this.grid[1][10].toString()  + "|" + this.grid[1][11].toString()  + "|" + this.grid[1][12].toString()  ;
+//        result += "|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|\n";
+//
+//
+//        return result;
+//    }
 }
